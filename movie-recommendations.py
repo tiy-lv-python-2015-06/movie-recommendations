5794a3ca21780e151fcfda967e9e55ee2d1676ca
Traci Armstrong <traciarms@gmail.com>
2015-07-12 23:09:33 -0700

    still working on trimming two user review lists to use the euclidean formula

--- movie-recommendations.py
+++ movie-recommendations.py
@@ -1,9 +1,10 @@
 import csv
 
+
 class Movie:
 
-    def __init__(self, id, name):
-        self.movie_id = id
+    def __init__(self, mid, name):
+        self.movie_id = mid
         self.ratings = []
         self.name = name
         self.ave_rating = 0
@@ -28,43 +29,117 @@ class Movie:
 
 class User:
 
-    def __init__(self, id):
-        self.user_id = id
-        self.user_ratings = []
+    def __init__(self, uid):
+        self.user_id = uid
+        self.user_movie_rating = []  # a list of tuples (movie id, rating)
+        self.movies_not_reviewed = []
+
+    def euclideanDistance(self, v, w):
+        """Given two lists, give the Euclidean distance between them on a scale
+        of 0 to 1. 1 means the two lists are identical.
+        """
+        # Guard against empty lists.
+        if len(v) is 0:
+            return 0
+
+        # Note that this is the same as vector subtraction.
+        differences = [v[idx] - w[idx] for idx in range(len(v))]
+        squares = [diff ** 2 for diff in differences]
+        sum_of_squares = sum(squares)
+
+        return 1 / (1 + math.sqrt(sum_of_squares))
+
+    def trim_rating_list(self, trim_to_list):
+
+        print('the length of this list is {} and the length of ' \
+              'the trim to list is {}' \
+              .format(len(self.user_movie_rating), len(trim_to_list)))
+
+        for i in self.user_movie_rating:
+            print(i)
+            print('is i in this list {}'. \
+                  format(i[0] not in self.user_movie_rating))
+            if i[0] not in trim_to_list:
+                print('we are deleting i {} from this list'.format(i))
+                #del self.user_movie_rating.index(i)
+                self.user_movie_rating.remove(i)
+                print('now the length is {}'.format(len(i)))
+
+        print('now the length of this list is {}'.
+              format(len(self.user_movie_rating)))
+        return self.user_movie_rating
 
     def __str__(self):
         return \
-            "user id: {} ratings: {}".format(self.user_id, self.user_ratings)
+            "user id: {} user movie rating tuple: {}".\
+                format(self.user_id, self.user_movie_rating)
 
-class Movie_Gen:
+class MovieGen:
 
-    def __init__(self):
-        pass
+    def __init__(self, user_id):
+        self.user_id = user_id
 
-    def find_user_dict(self, user_data_list, user_id):
-        # for each user
+    def find_user_list(self, user_data_list):
+        """
+            This function will create a list of user id indexed dictionaries
+            that contain a user object.
+        """
+        this_user_list = []
         the_user_dict = {}
         for data_set in user_data_list:
-            # we only need to make a new user if this user is not already in the
+            # we only need to make a new user if this user is not
+            # already in the
             # user dict
-            # we are only going to this user if its not the one wanting the reviews
-            if data_set['user id'] != user_id:
-                if data_set['user id'] not in the_user_dict:
-                    user = User(data_set['user id'])
-                else:
-                    user = the_user_dict[data_set['user id']]
+            if data_set['user id'] not in the_user_dict:
+                user = User(data_set['user id'])
+            else:
+                user = the_user_dict[data_set['user id']]
 
-                for u_dict in the_user_dict:
+            for u_dict in the_user_dict:
 
-                    if u_dict == data_set['user id']:
-                        user.user_ratings.append(data_set['rating'])
+                if u_dict == data_set['user id']:
 
-                # if this user id is not already in the dict
-                if data_set['user id'] not in the_user_dict:
-                    the_user_dict[data_set['user id']] = user
-                    #print('adding this user {}'.format(str(user)))
+                    user.user_movie_rating.append(
+                        (int(data_set['movie id']), data_set['rating']))
+
+            # if this user id is not already in the dict/list
+            if data_set['user id'] not in the_user_dict:
+                the_user_dict[data_set['user id']] = user
+                this_user_list.append(the_user_dict)
 
         return the_user_dict
+
+    def take_out_already_reviewed(self, the_movie_dict, user_obj):
+
+        #print('the users tuple is {}'.format(user_obj.user_movie_rating))
+        # if this user already rated this movie take it out of the list
+        for tup in user_obj.user_movie_rating:
+            del the_movie_dict[tup[0]]
+
+        #print('now the size of the list is {}'.format(len(the_movie_dict)))
+        return the_movie_dict
+
+    def find_movie_dict(self, movie_dict_list, user_data_list, user_obj):
+        # get each movie id out of the movie dict list
+        # we'll need a dictionary of movie objects with key movie id
+        the_movie_dict = {}
+        # we'll also need just a list of movie objects
+        the_movie_list = []
+        for each in movie_dict_list:
+            movie = Movie(each['movie id'], each['movie title'])
+
+            # Find all ratings for a movie by id
+            for user in user_data_list:
+                if user['movie id'] == each['movie id']:
+                    movie.ratings.append(int(user['rating']))
+
+            movie.find_ave_rating_with_min_num(min_reviews)
+            the_movie_list.append(movie)
+            the_movie_dict[(int(each['movie id']))] = movie
+
+        user_obj.movies_not_reviewed = the_movie_list
+        return the_movie_dict
+
 
 if __name__ == '__main__':
 
@@ -74,7 +149,8 @@ if __name__ == '__main__':
     min_reviews = \
         int(input('What is the minimum number of reviews you want to ' \
                   'include in your search?'))
-    user_id = input('What is your user ID?')
+    user_id1 = input('USER1:  What is your user ID?')
+    user_id2 = input('USER2:  What is your user ID?')
 
     md_header = ['movie id', 'movie title', 'release date', 'unknown', \
               'IMDb URL', 'genres1', 'genres2', 'genres3', \
@@ -96,71 +172,121 @@ if __name__ == '__main__':
         for row in reader:
             user_data_dict = dict(zip(ud_header, row))
             user_data_list.append(user_data_dict)
-            #print(user_data_dict)
 
-    user_dict1 = find_user_dict(user_data_list, user_id1)
-    user_dict2 = find_user_dict(user_data_list, user_id2
-                                )
+    user1_mov_gen = MovieGen(user_id1)
+    user2_mov_gen = MovieGen(user_id2)
+
+    user_dict1 = user1_mov_gen.find_user_list(user_data_list)
+    user_dict2 = user2_mov_gen.find_user_list(user_data_list)
+
+    the_movie_dict1 = \
+        user1_mov_gen.find_movie_dict(movie_dict_list, user_data_list,
+                                      user_dict1[user_id1])
+
+    the_movie_dict1 = user1_mov_gen.take_out_already_reviewed(
+        the_movie_dict1, user_dict1[user_id1])
+
+    print('the size of the movie gen movie list is {}'.
+          format(len(the_movie_dict1)))
+    print('the size of users review tuple is {}'.
+          format(len(user_dict1[user_id1].user_movie_rating)))
+
+    # sort the list from highest to lowest
+    # highest_ratings = \
+    #     sorted(the_movie_dict1, key=attrgetter('ave_rating'), reverse=True)
+    highest_ratings = sorted(user_dict1[user_id1].movies_not_reviewed, \
+                             key=lambda movie: movie.ave_rating, \
+                             reverse=True)
+
+    # get just the number the user requested
+    highest_ratings = highest_ratings[:num_movies]
+
+    print('USER1:  Your top {} movies are:\n'.format(num_movies))
+    for each in highest_ratings:
+        print('{}  with an average rating of {}'.
+              format(each.name, each.ave_rating, each.ratings))
+
+    the_movie_dict2 = \
+        user2_mov_gen.find_movie_dict(movie_dict_list, user_data_list,
+                                      user_dict2[user_id2])
+
+    the_movie_dict2 = user2_mov_gen.take_out_already_reviewed(
+        the_movie_dict2, user_dict2[user_id2])
+
+    print('the size of the movie gen movie list is {}'.
+          format(len(the_movie_dict2)))
+    print('the size of users review tuple is {}'.
+          format(len(user_dict2[user_id2].user_movie_rating)))
+
+    # sort the list from highest to lowest
+    highest_ratings = \
+        sorted(user_dict2[user_id2].movies_not_reviewed, \
+               key=lambda movie: movie.ave_rating, \
+               reverse=True)
+
+    # get just the number the user requested
+    highest_ratings = highest_ratings[:num_movies]
+
+    print('USER2:  Your top {} movies are:\n'.format(num_movies))
+    for each in highest_ratings:
+        print('{}  with an average rating of {}'.
+              format(each.name, each.ave_rating, each.ratings))
+
+    if len(user_dict1[user_id1].user_movie_rating) > \
+        len(user_dict2[user_id2].user_movie_rating):
+        trimmed_user1_list = user_dict1[user_id1].trim_rating_list(
+            user_dict2[user_id2].user_movie_rating)
+    else:
+        trimmed_user2_list = user_dict2[user_id2].trim_rating_list(
+            user_dict1[user_id1].user_movie_rating)
+
+    # lets find the euclidean distance between these two users:
+    # the_diff = user_dict1[user_id1]. \
+    #     euclideanDistance(user_dict1[user_id1].user_movie_rating ,
+    #                       user_dict2[user_id2].user_movie_rating)
+    #
+    # print('the euclidean different between user {} and user {} is {}'.
+    #       format(user_id1, user_id2, the_diff))
+
     # for each user
-    the_user_dict = {}
-    for data_set in user_data_list:
-        # we only need to make a new user if this user is not already in the
-        # user dict
-        # we are only going to this user if its not the one wanting the reviews
-        if data_set['user id'] != user_id:
-            if data_set['user id'] not in the_user_dict:
-                user = User(data_set['user id'])
-            else:
-                user = the_user_dict[data_set['user id']]
-
-            for u_dict in the_user_dict:
-
-                if u_dict == data_set['user id']:
-                    user.user_ratings.append(data_set['rating'])
-
-            # if this user id is not already in the dict
-            if data_set['user id'] not in the_user_dict:
-                the_user_dict[data_set['user id']] = user
-                #print('adding this user {}'.format(str(user)))
+    # the_user_dict = {}
+    # for data_set in user_data_list:
+    #     # we only need to make a new user if this user is not already in the
+    #     # user dict
+    #     # we are only going to this user if its not the one wanting the reviews
+    #     if data_set['user id'] != user_id:
+    #         if data_set['user id'] not in the_user_dict:
+    #             user = User(data_set['user id'])
+    #         else:
+    #             user = the_user_dict[data_set['user id']]
+    #
+    #         for u_dict in the_user_dict:
+    #
+    #             if u_dict == data_set['user id']:
+    #                 user.user_ratings.append(data_set['rating'])
+    #
+    #         # if this user id is not already in the dict
+    #         if data_set['user id'] not in the_user_dict:
+    #             the_user_dict[data_set['user id']] = user
+    #             #print('adding this user {}'.format(str(user)))
 
     # get each movie id out of the movie dict list
     # we'll need a dictionary of movie objects with key movie id
-    the_movie_dict = {}
-    # we'll also need just a list of movie objects
-    the_movie_list = []
-    for each in movie_dict_list:
-        movie = Movie(each['movie id'], each['movie title'])
-
-        # Find all ratings for a movie by id
-        for m_dict in user_data_list:
-
-            if m_dict['movie id'] == each['movie id']:
-                movie.ratings.append(int(m_dict['rating']))
-
-        movie.find_ave_rating_with_min_num(min_reviews)
-        the_movie_list.append(movie)
-        the_movie_dict[(int(each['movie id']))] = movie
-
-    # info = []
-    # new = []
-    # header2 = ['users', 'items', 'ratings']
-    # with open('./ml-100k/u.info') as file:
-    #     reader = csv.reader(file, delimiter="\n")
-    #     for row in reader:
-    #         info.append(row)
+    # the_movie_dict = {}
+    # # we'll also need just a list of movie objects
+    # the_movie_list = []
+    # for each in movie_dict_list:
+    #     movie = Movie(each['movie id'], each['movie title'])
     #
-    #     for item in range(len(info)):
-    #         idx = info[item][0].index(' ')
-    #         new.append(info[item][0][:idx])
+    #     # Find all ratings for a movie by id
+    #     for m_dict in user_data_list:
     #
-    #     movie_info_dict = dict(zip(header2, new))
-    #     print(movie_info_dict)
+    #         if m_dict['movie id'] == each['movie id']:
+    #             movie.ratings.append(int(m_dict['rating']))
+    #
+    #     movie.find_ave_rating_with_min_num(min_reviews)
+    #     the_movie_list.append(movie)
+    #     the_movie_dict[(int(each['movie id']))] = movie
 
-    highest_ratings = \
-        sorted(the_movie_list, key=lambda movie: movie.ave_rating, reverse=True)
-    highest_ratings = highest_ratings[:num_movies]
 
-    print('Your top {} movies are:'.format(num_movies))
-    for each in highest_ratings:
-        print('{}  with an average rating of {} and ratings list of {}'.
-              format(each.name, each.ave_rating, each.ratings))
+
